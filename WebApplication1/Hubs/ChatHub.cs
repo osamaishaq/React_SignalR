1aceaea642aefe5056e9075f18a79db8b5698d8d
Osama Ishaq <osama.ishaq@ibex.co>
2022-03-30 22:26:49 +0500

    first commit

---
+++ WebApplication1/Hubs/ChatHub.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly string _botUser;
+        private readonly IDictionary<string, UserConnection> _connections;
+        public ChatHub(IDictionary<string, UserConnection> connections)
+        {
+            _botUser = "MyChat Bot";
+            _connections = connections ?? throw new ArgumentNullException(nameof(connections));
+        }
+
+        public async Task SendMessage(string message)
+        {
+            if(_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+            {
+                await Clients.Group(userConnection.Room)
+                    .SendAsync("ReceiveMessage", userConnection.User, message);
+            }
+        }
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+
+            _connections[Context.ConnectionId] = userConnection;
+
+            await Clients.Groups(userConnection.Room).SendAsync("ReceiveMessage", _botUser, $"{userConnection.User} has Joined {userConnection.Room}");
+        }
+    }
+}
+ 
---
+++ WebApplication1/Models/UserConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class UserConnection
+    {
+        public string User { get; set; }
+        public string Room { get; set; }
+    }
+}
